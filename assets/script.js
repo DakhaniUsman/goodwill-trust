79cff7af5931f9be5a91f48570db411378ec275e
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-12 23:52:53 +0530

    updates

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function sideBar(){
+    // document.querySelectorAll(".mobile-navbar").style.display = "none";
+    // document.getElementById("navbar").style.display = "none";
+    console.log("clicked");
+    
+}
