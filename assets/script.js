0f4c5598960e42ff89fcfb27c33750164434ef81
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-13 19:31:12 +0530

    navbar

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-function sideBar(){
-    // document.querySelectorAll(".mobile-navbar").style.display = "none";
-    // document.getElementById("navbar").style.display = "none";
-    console.log("clicked");
-    
-}
+const hamMenu = document.querySelector(".hamMenu");
+
+const offScreenMenu = document.querySelector(".offScreenMenu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
